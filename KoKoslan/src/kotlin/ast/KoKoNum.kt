e15f2aa937f3bbdebbcdd17e8c43176f9393b584
Daniela Armas <daniela.armas.sanchez@est.una.ac.cr>
2017-10-29 00:48:03 -0600

    Mod, Num, Plus in ast

---
+++ KoKoslan/src/kotlin/ast/KoKoNum.kt
@@ -0,0 +1,14 @@
+/** 
+   Andrey Arguedas Espinoza
+   Daniela Armas Sanchez
+   Michael Chen Wang
+   Kimberly Olivas Delgado
+*/
+
+package kokoslan.ast
+
+class KoKoNum(value : Double) : KoKoAtom<Double>(value){
+   override fun eval(ctx : KoKoContext) : KoKoValue {
+	   return KoKoNumValue(getValue())
+   }
+}
