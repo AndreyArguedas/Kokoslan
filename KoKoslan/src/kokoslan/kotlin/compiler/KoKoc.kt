b9f412387fc8cbd03cccaa17679558344443876a
AndreyArguedas <andrey.arguedas.espinoza@est.una.ac.cr>
2017-11-02 23:46:02 -0600

    Working recursion

--- KoKoslan/src/kokoslan/kotlin/compiler/KoKoc.kt
+++ KoKoslan/src/kokoslan/kotlin/compiler/KoKoc.kt
@@ -27,7 +27,7 @@ fun main(args : Array<String>){
     println(">>> $VERSION <<<")
     println("...............................................\n")
     // Get parameters
-    val NOMBRE = "instantEvalLambda"
+    val NOMBRE = "recursion"
     var inputFile = "cases/$NOMBRE.kl"
     var outputFile = "output/$NOMBRE.kl.out.kl"
     /*
